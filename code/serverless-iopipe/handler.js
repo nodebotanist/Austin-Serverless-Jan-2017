6df1457799f34f6a69639a1ba4ce9467ea3d3483
Kas Perch <the@nodebotani.st>
2018-01-04 11:16:33 -0600

    Added IOpipe

--- code/serverless-iopipe/handler.js
+++ code/serverless-iopipe/handler.js
@@ -1,16 +1,19 @@
 'use strict';
+const iopipe = require('iopipe')({
+  token: 'MY_ACCESS_TOKEN'
+})
 
-module.exports.hello = (event, context, callback) => {
+module.exports.hello_iopipe = iopipe((event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
-    }),
-  };
+    })
+  }
 
   callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+})
